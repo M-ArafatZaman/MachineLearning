f8d44a5079d36d9bf55b9af10ddcd854f3592ac2
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-10-28 13:13:02 +0600

    Added static typing

--- src/LinearRegression/LinearRegression.py
+++ src/LinearRegression/LinearRegression.py
@@ -1,7 +1,11 @@
+from typing import List, Tuple, TypeVar
 
+## TYPES
+# Number type, union of int or float
+TNum = TypeVar("TNum", int, float)
 
 # Function to perform linear regression analysis
-def linearRegression(xs, ys):
+def linearRegression(xs: List[TNum], ys: List[TNum]) -> Tuple[TNum, TNum]:
     """
     This function takes in 2 vectors of ints or floats representing x and y respectively.
     And returns a list containing the gradient (m) in the first index and y-intercept (b) in the second index.
@@ -9,35 +13,27 @@ def linearRegression(xs, ys):
     The formula used is from Khan Academy - https://www.youtube.com/watch?v=GAmzwIkGFgE
     """
     # Size of dataset
-    N_x = len(xs)
-    N_y = len(ys)
+    N_x: int = len(xs)
+    N_y: int = len(ys)
 
     # Calculate the average of x and y
-    avg_x = sum(xs)/N_x
-    avg_y = sum(ys)/N_y
+    avg_x: TNum = sum(xs)/N_x
+    avg_y: TNum = sum(ys)/N_y
 
     # Calculate the average of a list of xys
-    xys = [xs[i]*ys[i] for i in range(N_x)]
-    avg_xy = sum(xys)/N_x
+    xys: List[TNum] = [xs[i]*ys[i] for i in range(N_x)]
+    avg_xy: TNum = sum(xys)/N_x
 
     # The square of average of x
-    squared_avg_x = pow(avg_x, 2)
+    squared_avg_x: TNum = pow(avg_x, 2)
 
     # Calculate the average of a list of xs squared
-    xs_squared = [pow(x, 2) for x in xs]
-    avg_xs_squared = sum(xs_squared)/N_x
+    xs_squared: List[TNum] = [pow(x, 2) for x in xs]
+    avg_xs_squared: TNum = sum(xs_squared)/N_x
 
     # Calculate the gradient
-    m = ((avg_x * avg_y) - avg_xy )/( squared_avg_x - avg_xs_squared )
-    b = (avg_y - (m * avg_x))
+    m: TNum = ((avg_x * avg_y) - avg_xy )/( squared_avg_x - avg_xs_squared )
+    b: TNum = (avg_y - (m * avg_x))
 
     return (m, b)
 
-
-if __name__ == "__main__":
-    x = [1,2,3,4,5]
-    y = [1,2,3,4,5]
-
-    m, b = linearRegression(x, y)
-
-    print(f"y={m}x+{b}")
